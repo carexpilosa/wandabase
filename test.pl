0c3cb672db5e96b8a77df107d9160e6a98b850b2
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-11-21 16:30:43 +0100

    dev: database connection

--- test.pl
+++ test.pl
@@ -1,18 +1,23 @@
-#!/env/perl
+#!/usr/bin/perl -w
 
 use strict;
+
 use DBI;
 use DBD::mysql;
 use Data::Dumper;
 
-my $dsn = "DBI:mysql:database=wandabase;host=localhost";
-my $dbh = DBI->connect($dsn, 'markus', 'markus');
+print "Content-Type: text/html\n\n";
+print "Hello World";
+
+my $dsn = "DBI:mysql:database=wanderbase;host=localhost";
+my $dbh = DBI->connect($dsn, 'root', 'zM0dem');
 
 my $sth = $dbh->prepare('SELECT * FROM members')
   or die "prepare statement failed: $dbh->errstr()";
 $sth->execute();
 
 while (my $ref = $sth->fetchrow_arrayref()) {
+  warn Data::Dumper->Dump ($ref);
   print Data::Dumper->Dump ($ref);
 }
 
