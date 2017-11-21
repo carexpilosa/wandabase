df58cae28a464d65b133132b0599a8daa4e78972
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-11-21 22:47:36 +0100

    dev: account anlegen

--- sql.sql
+++ sql.sql
@@ -5,6 +5,9 @@ CREATE TABLE members (
 
 INSERT INTO members (id, username) VALUES (1, 'horst');
 
+INSERT INTO members (id, username, password, gender, date_of_membership, is_admin)
+              VALUES(2, 'meyerklein', '123', 'm', '2017-11-21', 0);
+
 create USER 'markus'@'localhost' IDENTIFIED BY 'markus';
 GRANT ALL PRIVILEGES ON meineDB . * TO 'markus'@'localhost';
 
--- test.pl
+++ test.pl
@@ -11,20 +11,15 @@ print "Content-Type: text/html\n\n";
 
 my $cgi = CGI->new();
 my @val = $cgi->param();
-map {
-  print $_ . " => " . $cgi->param($_) . "<br />\n";
-} @val;
 
 my $dsn = "DBI:mysql:database=wanderbase;host=localhost";
 my $dbh = DBI->connect($dsn, 'root', 'zM0dem');
-
-#my $sth = $dbh->prepare('SELECT * FROM members')
-#  or die "prepare statement failed: $dbh->errstr()";
-#INSERT INTO members (id, username, password, gender, date_of_membership, is_admin)
-#  VALUES(?, ?, ?, ?, ?, ?);
-#id, username, password, gender, date_of_membership, is_admin
-#$sth->execute();
-
-#while (my $ref = $sth->fetchrow_arrayref()) {
-#  map { print $_ . "<br />\n"} @{$ref};
-#}
+my $statement = 'INSERT INTO members (id, username, password, gender, date_of_membership, is_admin)'
+  . ' VALUES(?, ?, ?, ?, ?, ?)';
+my $sth = $dbh->prepare($statement);
+my @paramKeys = qw (id username password gender date_of_membership is_admin);
+my @bindValues = map {
+  $cgi->param($_) || '0';
+} @paramKeys;
+print join(',', @bindValues);
+$sth->execute(@bindValues);
