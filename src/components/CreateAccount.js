b69c1b694de973ff3e81002c89ff33a14c462415
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-11-24 15:03:13 +0100

    dev: rest create account

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -8,7 +8,7 @@ export default class CreateAccount extends React.Component {
     return (
       <div>
         Neuer Account
-        <form action="http://localhost/wanda/test.pl" method="post">
+        { /*<form action="http://localhost/wanda/srvrest.pl" method="post"> */ }
           <label htmlFor="username">Username: </label>
           <input name="username" type="text" />
           <br />
@@ -27,9 +27,34 @@ export default class CreateAccount extends React.Component {
           <label htmlFor="motto">Motto</label>
           <textarea name="motto" rows="4" cols="50"></textarea>
           <br />
-          <input type="submit" />
-        </form>
+          <button onClick={(e) => { this._do(e) } }>CLICK</button>
+        { /* </form> */ }
       </div>
     );
+  }
+
+  _do (e) {
+    console.log(e.target);
+    let url = 'http://localhost/wanda/srvrest.pl';
+    fetch(url, {
+      method: 'post',
+      body: JSON.stringify({
+        "username": "Fritz",
+        "password": "Manfred",
+        "gender": "f",
+        "is_admin": "on",
+        "motto": "Hol raus, was rauszuholen geht"
+      })
+    }) // Call the fetch function passing the url of the API as a parameter
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json);
+    })
+    .catch(function(error) {
+      // This is where you run code if the server returns any errors
+      console.log('ERROR: ' + error);
+    });
   }
 }
--- srvrest.pl
+++ srvrest.pl
@@ -42,11 +42,17 @@ if( $request_method eq 'GET' ) {
   $rest_data .= "GET";
 
 
-  $rest_data .= to_json({
+  $rest_data = to_json({
     'ein' => 'test',
-    'als' => 'hash'
+    'als' => 'hash',
+    'meth' => 'GET'
   });
-  $rest_data = $page->header('application/json;charset=UTF-8') . "OK\n" . $rest_data;
+  $rest_data = $page->header(
+    -content_type => 'application/json;charset=UTF-8',
+    -access_control_allow_origin => '*') . $rest_data;
+  #$rest_data = $page->header('application/json;charset=UTF-8') . "OK\n" . $rest_data;
+
+
   #if( not exists $data{ $id } ){
   #  $rest_data = $page->header('text/html', '404 Not found') . $rest_data;
   #} else {
@@ -56,30 +62,41 @@ if( $request_method eq 'GET' ) {
   #  $rest_data =  $page->header('text/html') . "OK\n" . $rest_data;
   #}
 } elsif ( $request_method eq 'POST' ) { # ----- Daten schreiben
-    my $data = from_json( $page->param( 'POSTDATA' ) );
-    $rest_data .= Data::Dumper::Dumper $data;
-
-
-    my @bindValues;
-    map {
-      #$rest_data .= $data->{$_} . "<br />\n";
-      $rest_data .= "KEY => $_<br/>\n";
-      my $ret;
-      if ($_ eq 'is_admin') {
-        $ret = $data->{$_} && $data->{$_} eq 'on' ? 1 : 0;
-      } elsif($_ eq 'motto') {
-        $ret = $data->{$_} || '';
-      } elsif($_ eq 'date_of_membership') {
-        $ret = strftime('%Y-%m-%d', localtime);
-      } else {
-        $ret = $data->{$_};
-      }
-      push @bindValues, $ret;
-    } keys %$data;
-    $rest_data .= Data::Dumper::Dumper \@bindValues;
+    $rest_data = to_json({
+      'ein' => 'testPOST',
+      'als' => 'hashPOST',
+      'meth' => 'POST',
+      'postdata' => $page->param( 'POSTDATA' )
+    });
+    $rest_data = $page->header(
+      -content_type => 'application/json;charset=UTF-8',
+      -access_control_allow_origin => '*') . $rest_data;
+    
+    #$rest_data .= $page->param( 'POSTDATA' );
+    #my $data = from_json( $page->param( 'POSTDATA' ) );
+    #$rest_data .= Data::Dumper::Dumper $data;
+#
+#
+    #my @bindValues;
+    #map {
+    #  #$rest_data .= $data->{$_} . "<br />\n";
+    #  $rest_data .= "KEY => $_<br/>\n";
+    #  my $ret;
+    #  if ($_ eq 'is_admin') {
+    #    $ret = $data->{$_} && $data->{$_} eq 'on' ? 1 : 0;
+    #  } elsif($_ eq 'motto') {
+    #    $ret = $data->{$_} || '';
+    #  } elsif($_ eq 'date_of_membership') {
+    #    $ret = strftime('%Y-%m-%d', localtime);
+    #  } else {
+    #    $ret = $data->{$_};
+    #  }
+    #  push @bindValues, $ret;
+    #} keys %$data;
+    #$rest_data .= Data::Dumper::Dumper \@bindValues;
     # Daten in den DB_File Hash schreiben
     #$data{ $book->{id} } = "$book->{title};$book->{author}";
-    $rest_data =  $page->header('text/html') . "OK\n" . $rest_data;
+    #$rest_data = $page->header('text/html') . "OK\n" . $rest_data;
 } else {
   $rest_data = $page->header('text/html', '404 Not found') . $rest_data;
 }
