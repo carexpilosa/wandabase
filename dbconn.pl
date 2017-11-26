db86aec29b50838ba02c2f4750fa8f94cc4ea929
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-11-26 19:07:06 +0100

    CreateAccount to db via json

---
+++ dbconn.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+use CGI;
+use DBI;
+use DB_File;
+use Data::Dumper;
+use POSIX qw(strftime);
+use JSON;
+
+my $page  = new CGI;
+
+#print "Content-type: text/html \n\n";
+
+my $rest_data; # = $page->header('application/json;charset=UTF-8');
+#print $page->header('application/json');
+# ------ Initialisierung des Datenzugriffs über DB_File ------
+#my $file = "./books.data";
+#my %data;
+#my $db = tie %data, "DB_File", $file or die "err open $file: $!\n";
+
+# ------ Prüfen ob über den definierten Pfad auf den REST Service zugegriffen wurde ------
+my $path_info = $ENV{ 'PATH_INFO' };
+
+#$rest_data = $page->header('text/html');
+#$rest_data .= "$path_info<br />\n";
+
+
+$path_info =~ /^\/(.+)?\/(.*)$/;
+my $type = $1;
+my $id = $2;
+
+#if( not $type eq 'bookdb' ){
+#  exit;
+#}
+
+# ------ Zugriffsmethode ermitteln
+my $request_method = $ENV{ 'REQUEST_METHOD' };
+$rest_data .= "REQUEST_METHOD => $request_method\n";
+
+if( $request_method eq 'GET' ) {
+  $rest_data .= "GET";
+
+
+  $rest_data = to_json({
+    'ein' => 'test',
+    'als' => 'hash',
+    'meth' => 'GET'
+  });
+  $rest_data = $page->header(
+    -content_type => 'application/json;charset=UTF-8',
+    -access_control_allow_origin => '*') . $rest_data;
+} elsif ( $request_method eq 'POST' ) { # ----- Daten schreiben
+    my $data = from_json($page->param( 'POSTDATA' ));
+    $data->{'date_of_membership'} = strftime('%Y-%m-%d', localtime);
+    warn "DATA => ".Data::Dumper::Dumper $data;
+    my @bindValues;
+    map {
+      my $ret;
+      if ($_ eq 'is_admin') {
+        $ret = $data->{$_} && $data->{$_} eq 'on' ? 1 : 0;
+      } elsif($_ eq 'motto') {
+        $ret = $data->{$_} || '';
+      } elsif($_ eq 'date_of_membership') {
+        $ret = strftime('%Y-%m-%d', localtime);
+      } else {
+        $ret = $data->{$_};
+      }
+      push @bindValues, $ret;
+    } ('username', 'password', 'gender', 'date_of_membership', 'is_admin', 'motto');
+
+
+    my $dsn = "DBI:mysql:database=wanderbase;host=localhost";
+    my $dbh = DBI->connect($dsn, 'root', 'zM0dem');
+    my $statement = 'INSERT INTO members (
+        username, password, gender, date_of_membership, is_admin, motto
+      ) VALUES(?, ?, ?, ?, ?, ?)';
+    my $sth = $dbh->prepare($statement);
+    warn $statement . " => " . join ', ', @bindValues;
+    $sth->execute(@bindValues);
+
+    $rest_data = $page->header(
+      -content_type => 'application/json;charset=UTF-8',
+      -access_control_allow_origin => '*'
+    ) . to_json($data);
+    
+    
+} else {
+  $rest_data = $page->header('text/html', '404 Not found') . $rest_data;
+}
+#$rest_data = $page->header('text/html') . "OK\n";
+print $rest_data; # http Response ausgeben
--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -8,45 +8,44 @@ export default class CreateAccount extends React.Component {
     return (
       <div>
         Neuer Account
-        { /*<form action="http://localhost/wanda/srvrest.pl" method="post"> */ }
           <label htmlFor="username">Username: </label>
-          <input name="username" type="text" />
+          <input name="username" id="username" type="text" />
           <br />
           <label htmlFor="password">Password: </label>
-          <input name="password" type="password" />
+          <input name="password" id="password" type="password" />
           <br />
           <label htmlFor="gender">Gender: </label>
-          <select name="gender" type="select">
+          <select name="gender" id="gender" type="select">
             <option>m</option>
-            <option>w</option>
+            <option>f</option>
           </select>
           <br />
           <label htmlFor="isadmin">Is Admin</label>
-          <input type="checkbox" name="is_admin" />
+          <input type="checkbox" name="is_admin" id="is_admin" />
           <br />
           <label htmlFor="motto">Motto</label>
-          <textarea name="motto" rows="4" cols="50"></textarea>
+          <textarea name="motto" id="motto" rows="4" cols="50"></textarea>
           <br />
           <button onClick={(e) => { this._do(e) } }>CLICK</button>
-        { /* </form> */ }
       </div>
     );
   }
 
   _do (e) {
     console.log(e.target);
-    let url = 'http://localhost/wanda/srvrest.pl';
+    let url = 'http://localhost/wanda/dbconn.pl';
     fetch(url, {
       method: 'post',
       body: JSON.stringify({
-        "username": "Fritz",
-        "password": "Manfred",
-        "gender": "f",
-        "is_admin": "on",
-        "motto": "Hol raus, was rauszuholen geht"
+        "username": document.getElementById('username').value,
+        "password": document.getElementById('password').value,
+        "gender": document.getElementById('gender').value,
+        "is_admin": document.getElementById('is_admin').value,
+        "motto": document.getElementById('motto').value
       })
     }) // Call the fetch function passing the url of the API as a parameter
     .then(function(response) {
+      console.log(response);
       return response.json();
     })
     .then(function(json) {
