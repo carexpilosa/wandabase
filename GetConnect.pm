5d0a6696d3470085a3a1a058c729184f35c834bb
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-11-29 23:06:49 +0100

    dev GetConnect Module

--- GetConnect.pm
+++ GetConnect.pm
@@ -11,36 +11,39 @@ sub getDbQuery {
   my ($dbh, $type, $id, $page) = @_;
   my $data = from_json($page->param( 'POSTDATA' ));
   my $rest_data;
-  if ($type eq 'members' && $id eq 'new') {
-    $data->{'date_of_membership'} = strftime('%Y-%m-%d', localtime);
-    my @bindValues;
-    map {
-      my $ret;
-      if ($_ eq 'is_admin') {
-        $ret = $data->{$_} && $data->{$_} eq 'on' ? 1 : 0;
-      } elsif($_ eq 'motto') {
-        $ret = $data->{$_} || '';
-      } elsif($_ eq 'date_of_membership') {
-        $ret = strftime('%Y-%m-%d', localtime);
-      } else {
-        $ret = $data->{$_};
-      }
-      push @bindValues, $ret;
-    } ('username', 'password', 'gender', 'date_of_membership', 'is_admin', 'motto');
-
-    my $statement = 'INSERT INTO members (
-        username, password, gender, date_of_membership, is_admin, motto
-      ) VALUES(?, ?, ?, ?, ?, ?)';
-    my $sth = $dbh->prepare($statement);
-    my $success = $sth->execute(@bindValues);
-
-    $data->{'success'} = $success;
-
-    $rest_data = $page->header(
-      -content_type => 'application/json;charset=UTF-8',
-      -access_control_allow_origin => '*'
-    ) . to_json($data);
+  my $statement = <<EOT;
+      SELECT id, username, password, gender, date_of_membership, is_admin, motto
+        FROM members;
+EOT
+  
+  if ($type eq 'members' && $id eq 'all') {  
+    my $query = $dbh->prepare($statement);
+    $query->execute() or die $query->err_str;
+    my %result;
+    while (my ($id, $username, $password, $gender, $date_of_membership, $is_admin, $motto) =
+      $query->fetchrow_array()) {
+         $result{$id} = {
+          id => $id,
+          username => $username,
+          password => $password,
+          gender => $gender, 
+          date_of_membership => $date_of_membership, 
+          is_admin => $is_admin, 
+          motto => $motto
+        }
+    }
+    $rest_data = to_json(\%result);
+  } else {
+    $rest_data = to_json({
+      'ein' => 'test',
+      'als' => 'hash',
+      'meth' => 'GET'
+    });
   }
+  
+  $rest_data = $page->header(
+    -content_type => 'application/json;charset=UTF-8',
+    -access_control_allow_origin => '*') . $rest_data;
   return $rest_data;
 }
 
