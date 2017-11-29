024c2d09b8f4582682791ac803d2c041628491b6
Bernhard Prott <bernhard.prott@thinkproject.com>
2017-11-29 10:35:40 +0100

    test branch

--- PostConnect.pm
+++ PostConnect.pm
@@ -4,43 +4,44 @@ use JSON;
 use CGI;
 use DBI;
 
-sub helloPost {
-  my ($id, $type) = @_;
+sub postDbQuery {
+  my ($dbh, $id, $type) = @_;
+  warn "-----------------";
   my $page  = new CGI;
-  if ($type eq 'members' && $id eq 'all') {
-    my $dsn = "DBI:mysql:database=wanderbase;host=localhost";
-    my $dbh = DBI->connect($dsn, 'markus', 'markus');
-    my $statement = <<EOT;
-      SELECT id, username, password, gender, date_of_membership, is_admin, motto
-        FROM members;
-EOT
-    my $query = $dbh->prepare($statement);
-    $query->execute() or die $query->err_str;
-    my %result;
-    while (my ($id, $username, $password, $gender, $date_of_membership, $is_admin, $motto) =
-      $query->fetchrow_array()) {
-         $result{$id} = {
-          id => $id,
-          username => $username,
-          password => $password,
-          gender => $gender, 
-          date_of_membership => $date_of_membership, 
-          is_admin => $is_admin, 
-          motto => $motto
-        }
-    }
-    $rest_data = to_json(\%result);
-  } else {
-    $rest_data = to_json({
-      'ein' => 'test',
-      'als' => 'hash',
-      'meth' => 'GET'
-    });
+  my $data = from_json($page->param( 'POSTDATA' ));
+  my $rest_data;
+  if ($type eq 'members' && $id eq 'new') {
+    $data->{'date_of_membership'} = strftime('%Y-%m-%d', localtime);
+    my @bindValues;
+    map {
+      my $ret;
+      if ($_ eq 'is_admin') {
+        $ret = $data->{$_} && $data->{$_} eq 'on' ? 1 : 0;
+      } elsif($_ eq 'motto') {
+        $ret = $data->{$_} || '';
+      } elsif($_ eq 'date_of_membership') {
+        $ret = strftime('%Y-%m-%d', localtime);
+      } else {
+        $ret = $data->{$_};
+      }
+      push @bindValues, $ret;
+    } ('username', 'password', 'gender', 'date_of_membership', 'is_admin', 'motto');
+
+
+    my $statement = 'INSERT INTO members (
+        username, password, gender, date_of_membership, is_admin, motto
+      ) VALUES(?, ?, ?, ?, ?, ?)';
+    warn "############# $statement";
+    my $sth = $dbh->prepare($statement);
+    my $success = $sth->execute(@bindValues);
+
+    $data->{'success'} = $success;
+
+    $rest_data = $page->header(
+      -content_type => 'application/json;charset=UTF-8',
+      -access_control_allow_origin => '*'
+    ) . to_json($data);
   }
-  
-  $rest_data = $page->header(
-    -content_type => 'application/json;charset=UTF-8',
-    -access_control_allow_origin => '*') . $rest_data;
   return $rest_data;
 }
 
--- dbconn.pl
+++ dbconn.pl
@@ -10,8 +10,9 @@ use Data::Dumper;
 use POSIX qw(strftime);
 use JSON;
 
+use PostConnect;
+
 my $page  = new CGI;
-my $rest_data; # = $page->header('application/json;charset=UTF-8');
 
 # ------ Prüfen ob über den definierten Pfad auf den REST Service zugegriffen wurde ------
 my $path_info = $ENV{ 'PATH_INFO' };
@@ -22,14 +23,14 @@ if ($path_info =~ /^\/(.+)?\/(.*)$/) {
 }
 
 my $request_method = $ENV{ 'REQUEST_METHOD' };
-$rest_data .= "REQUEST_METHOD => $request_method\n";
+
+my $dsn = "DBI:mysql:database=wanderbase;host=localhost";
+my $dbh = DBI->connect($dsn, 'markus', 'markus');
+my $rest_data = '';
 
 if( $request_method eq 'GET' ) {
-  $rest_data .= "GET";
-
   if ($type eq 'members' && $id eq 'all') {
-    my $dsn = "DBI:mysql:database=wanderbase;host=localhost";
-    my $dbh = DBI->connect($dsn, 'markus', 'markus');
+    
     my $statement = <<EOT;
       SELECT id, username, password, gender, date_of_membership, is_admin, motto
         FROM members;
@@ -61,41 +62,9 @@ EOT
   $rest_data = $page->header(
     -content_type => 'application/json;charset=UTF-8',
     -access_control_allow_origin => '*') . $rest_data;
-} elsif ( $request_method eq 'POST' && $type eq 'members' && $id eq 'new') { # ----- Daten schreiben
-    my $data = from_json($page->param( 'POSTDATA' ));
-    $data->{'date_of_membership'} = strftime('%Y-%m-%d', localtime);
-    my @bindValues;
-    map {
-      my $ret;
-      if ($_ eq 'is_admin') {
-        $ret = $data->{$_} && $data->{$_} eq 'on' ? 1 : 0;
-      } elsif($_ eq 'motto') {
-        $ret = $data->{$_} || '';
-      } elsif($_ eq 'date_of_membership') {
-        $ret = strftime('%Y-%m-%d', localtime);
-      } else {
-        $ret = $data->{$_};
-      }
-      push @bindValues, $ret;
-    } ('username', 'password', 'gender', 'date_of_membership', 'is_admin', 'motto');
-
-
-    my $dsn = "DBI:mysql:database=wanderbase;host=localhost";
-    my $dbh = DBI->connect($dsn, 'markus', 'markus');
-    my $statement = 'INSERT INTO members (
-        username, password, gender, date_of_membership, is_admin, motto
-      ) VALUES(?, ?, ?, ?, ?, ?)';
-    my $sth = $dbh->prepare($statement);
-    my $success = $sth->execute(@bindValues);
-
-    $data->{'success'} = $success;
-
-    $rest_data = $page->header(
-      -content_type => 'application/json;charset=UTF-8',
-      -access_control_allow_origin => '*'
-    ) . to_json($data);
-    
-    
+} elsif ( $request_method eq 'POST') {
+    $rest_data = PostConnect::postDbQuery($dbh, $type, $id);
+    warn $rest_data;
 } else {
   $rest_data = $page->header('text/html', '404 Not found') . $rest_data;
 }
