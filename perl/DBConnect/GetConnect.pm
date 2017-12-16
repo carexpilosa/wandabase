49c9342b54642feff95a6b5fb883fbde1444f890
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-12-17 00:16:20 +0100

    refactoring of GetConnect

--- perl/DBConnect/GetConnect.pm
+++ perl/DBConnect/GetConnect.pm
@@ -28,14 +28,8 @@ sub getDbQuery {
     } else {
       die "wrong entity type $type";
     }
-    $restData = Encode::encode_utf8(to_json($result));
-    $restData = $page->header(
-      -content_type => 'application/json;charset=UTF-8',
-      -access_control_allow_origin => '*',
-      -access_control_allow_methods => 'GET,HEAD,OPTIONS,POST,PUT',
-      -access_control_allow_headers => 'Mode, Token, Origin, X-Requested-With, Content-Type, Accept',
-      -content_type => 'application/json;charset=UTF-8',
-      -status => '200 OK') . $restData;
+    warn "xxx";
+    warn Dumper $result;
   } elsif ($type =~ /^(members|events|comments)$/ && $id && $id =~ /^\d+$/) {
     if ($type eq 'members') {
       $result = Entities::Members->getMemberForId($id);
@@ -46,71 +40,60 @@ sub getDbQuery {
     } else {
       die "wrong entity type $type";
     }
-    $restData = Encode::encode_utf8(to_json($result));
-    $restData = $page->header(
-      -content_type => 'application/json;charset=UTF-8',
-      -access_control_allow_origin => '*',
-      -access_control_allow_methods => 'GET,HEAD,OPTIONS,POST,PUT',
-      -access_control_allow_headers => 'Mode, Token, Origin, X-Requested-With, Content-Type, Accept',
-      -content_type => 'application/json;charset=UTF-8',
-      -status => '200 OK') . $restData;
+    warn Dumper $result;
   } elsif ($type eq 'comments' && $eventID) {
     $result = Entities::Comments
       ->getCommentsOfMembersForEventIdAsHash($eventID);
-    $restData = Encode::encode_utf8(to_json($result));
-    $restData = $page->header(
-      -content_type => 'application/json;charset=UTF-8',
-      -access_control_allow_origin => '*',
-      -access_control_allow_methods => 'GET,HEAD,OPTIONS,POST,PUT',
-      -access_control_allow_headers => 'Mode, Token, Origin, X-Requested-With, Content-Type, Accept',
-      -content_type => 'application/json;charset=UTF-8',
-      -status => '200 OK') . $restData;
-  } elsif ($type =~ /^(members|events)$/) {
-    my $entity = $type eq 'members'
-        ? Entities::Members->new()
-        : Entities::Events->new();
-    my $sortedFieldNamesForGet = $entity->sortedFieldNamesForGet();
-    my $colnames = join (', ', @{$sortedFieldNamesForGet});
-    if ($id eq 'all') {
-      $statement = "SELECT $colnames FROM $type";
-    } elsif ($id =~ /^\d+$/) { #single entity called with id
-      $statement = "SELECT $colnames FROM $type WHERE id=$id";
-    } else {
-      $restData = DBConnect::Connect::errorResponse($page);
-    }
-    
-    my $dbRes = DBConnect::DBWorker::doGet($dbh, $statement, []);
-    my %result = map { $_->{'id'} => $_ } @{$dbRes};
-    $restData = Encode::encode_utf8(to_json(\%result));
-    $restData = $page->header(
-      -content_type => 'application/json;charset=UTF-8',
-      -access_control_allow_origin => '*',
-      -access_control_allow_methods => 'GET,HEAD,OPTIONS,POST,PUT',
-      -access_control_allow_headers => 'Mode, Token, Origin, X-Requested-With, Content-Type, Accept',
-      -content_type => 'application/json;charset=UTF-8',
-      -status => '200 OK') . $restData;
-  } elsif ($type eq 'comments') {
-    my $eventID = $page->param('event_id');
-    my $statement = <<EOT;
-      SELECT comments.id, comments.content, members.username, comments.created
-        FROM comments, members
-          WHERE comments.event_id = ?
-            AND comments.member_id = members.id
-EOT
-    my $dbRes = DBConnect::DBWorker::doGet($dbh, $statement, [$eventID]);
-    my %result = map { $_->{'id'} => $_ } @{$dbRes};
-    $restData = Encode::encode_utf8(to_json(\%result));
-    $restData = $page->header(
-      -content_type => 'application/json;charset=UTF-8',
-      -access_control_allow_origin => '*',
-      -access_control_allow_methods => 'GET,HEAD,OPTIONS,POST,PUT',
-      -access_control_allow_headers => 'Mode, Token, Origin, X-Requested-With, Content-Type, Accept',
-      -content_type => 'application/json;charset=UTF-8',
-      -status => '200 OK') . $restData;
+    warn "xxx";
+    warn Dumper $result;
+  #} elsif ($type =~ /^(members|events)$/) {
+  #  my $entity = $type eq 'members'
+  #      ? Entities::Members->new()
+  #      : Entities::Events->new();
+  #  my $sortedFieldNamesForGet = $entity->sortedFieldNamesForGet();
+  #  my $colnames = join (', ', @{$sortedFieldNamesForGet});
+  #  if ($id eq 'all') {
+  #    $statement = "SELECT $colnames FROM $type";
+  #  } elsif ($id =~ /^\d+$/) { #single entity called with id
+  #    $statement = "SELECT $colnames FROM $type WHERE id=$id";
+  #  } else {
+  #    return DBConnect::Connect::errorResponse($page);
+  #  }
+  #  
+  #  my $dbRes = DBConnect::DBWorker::doGet($dbh, $statement, []);
+  #  my %result = map { $_->{'id'} => $_ } @{$dbRes};
+  #} elsif ($type eq 'comments') {
+  #  my $eventID = $page->param('event_id');
+  #  my $statement = <<EOT;
+  #    SELECT comments.id, comments.content, members.username, comments.created
+  #      FROM comments, members
+  #        WHERE comments.event_id = ?
+  #          AND comments.member_id = members.id
+#EOT
+  #  my $dbRes = DBConnect::DBWorker::doGet($dbh, $statement, [$eventID]);
+  #  my $result = [ map { $_->{'id'} => $_ } @{$dbRes} ];
   } else {
-    $restData = DBConnect::Connect::errorResponse($page);
+    warn "xxx";
+    return DBConnect::Connect::errorResponse($page);
   }
-  return $restData;  
+  warn "xxx";
+  warn Dumper $result;
+  return createRestDataFromResultHash($result, $page);
+}
+
+sub createRestDataFromResultHash {
+  my ($result, $page) = @_;
+  warn "xxx";
+  warn Dumper $result;
+  my $restData = Encode::encode_utf8(to_json($result));
+  $restData = $page->header(
+    -content_type => 'application/json;charset=UTF-8',
+    -access_control_allow_origin => '*',
+    -access_control_allow_methods => 'GET,HEAD,OPTIONS,POST,PUT',
+    -access_control_allow_headers => 'Mode, Token, Origin, X-Requested-With, Content-Type, Accept',
+    -content_type => 'application/json;charset=UTF-8',
+    -status => '200 OK') . $restData;
+  return $restData;
 }
 
 1;
